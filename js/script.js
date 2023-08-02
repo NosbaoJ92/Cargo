9a0270335798e3929764a70885b63600a4459262
NosbaoJ92 <joabson.nogueira@hotmail.com>
2023-08-02 17:01:36 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Start AbrirMenu
+function AbrirMenu() {
+    document.getElementById("divLinks2").style.display = "block";
+    document.getElementById("btnAbrirMenuIcon").style.display = "none";
+    document.getElementById("btnFecharMenuIcon").style.display = "block";
+    document.getElementById("divLinks").style.display="none";
+    }
+// End AbrirMenu
+// Start FecharMenu
+function FecharMenu() {
+    document.getElementById("divLinks2").style.display = "none";
+    document.getElementById("btnFecharMenuIcon").style.display = "none";
+    document.getElementById("btnAbrirMenuIcon").style.display = "block";
+    document.getElementById("Logo_text").style.display="block";
+}
+// End FecharMenu
